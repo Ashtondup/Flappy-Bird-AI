7563de998633825f83870c7895d4e2256a551853
Ashtondup <ashtondup@gmail.com>
2023-11-07 15:17:40 +0200

    Correct errors
    Corrected the errors that I made wile typing

--- Flappy-Bird/population.py
+++ Flappy-Bird/population.py
@@ -28,6 +28,12 @@ class Population:
         print('CALCULATE FITNESS')
         self.calculate_fitness()
         
+        print('KILL EXTINCT')
+        self.kill_extinct_species()
+        
+        print('KILL STALE')
+        self.kill_stale_species()
+        
         print('SORT BY FITNESS')
         self.sort_species_by_fitness()
         
@@ -54,9 +60,33 @@ class Population:
         for s in self.species:
             s.calculate_average_fitness()
             
+    def kill_extinct_species(self):
+        species_bin = []
+        for s in self.species:
+            if len(s.players) == 0:
+                species_bin.append(s)
+        for s in species_bin:
+            self.species.remove(s)
+            
+    def kill_stale_species(self):
+        player_bin = []
+        species_bin = []
+        for s in self.species:
+            if s.staleness >=8:
+                if len(self.species) > len(species_bin) + 1:
+                    species_bin.append(s)
+                    for p in s.players:
+                        player_bin.append(p)
+                else:
+                    s.staleness = 0
+        for p in player_bin:
+            self.players.remove(p)
+        for s in species_bin:
+            self.species.remove(s)
+            
     def sort_species_by_fitness(self):
         for s in self.species:
-            s.sort_species_by_fitness()
+            s.sort_players_by_fitness()
             
         self.species.sort(key=operator.attrgetter('benchmark_fitness'), reverse=True)
         
